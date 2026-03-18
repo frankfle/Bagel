f40f515aae37288401597c53f71aac22f10d2e29
Frank Fleschner <frankfle@me.com>
2026-03-17 20:43:00 -0500

    Added blank project for a test implementation

---
+++ test/test/testApp.swift
@@ -0,0 +1,17 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Frank Fleschner on 3/17/26.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

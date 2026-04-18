7830a0260f15717f3648c2893fbdc00dbad299f2
Frank Fleschner <frankfle@me.com>
2026-04-17 20:18:16 -0500

    Add dock badge showing packet count when app is backgrounded
    Subscribe to didGetPacket notification in AppDelegate to increment a
    counter and set dockTile.badgeLabel when the app is not in the
    foreground. Clear badge and reset counter when app becomes active.

--- mac/Bagel/AppDelegate.swift
+++ mac/Bagel/AppDelegate.swift
@@ -10,14 +10,39 @@ import Cocoa
 
 class AppDelegate: NSObject, NSApplicationDelegate {
 
+    private var isAppActive = true
+    private var backgroundPacketCount = 0
+    private var packetObserver: NSObjectProtocol?
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         for window in NSApplication.shared.windows {
             window.standardWindowButton(.closeButton)?.isEnabled = false
+        }
+        packetObserver = NotificationCenter.default.addObserver(
+            forName: BagelNotifications.didGetPacket,
+            object: nil,
+            queue: .main
+        ) { [weak self] _ in
+            guard let self = self else { return }
+            if !self.isAppActive {
+                self.backgroundPacketCount += 1
+                NSApplication.shared.dockTile.badgeLabel = String(self.backgroundPacketCount)
+            }
         }
     }
 
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
+    }
+
+    func applicationDidBecomeActive(_ notification: Notification) {
+        isAppActive = true
+        NSApplication.shared.dockTile.badgeLabel = ""
+        backgroundPacketCount = 0
+    }
+
+    func applicationWillResignActive(_ notification: Notification) {
+        isAppActive = false
     }
 
     func applicationShouldHandleReopen(_ sender: NSApplication, hasVisibleWindows flag: Bool) -> Bool {
